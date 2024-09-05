e9dabcc65e03508199bb58f7c22ca543ea865a31
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-09-05 20:57:10 +0530

    .

---
+++ dll.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *prev;
+        Node *next;
+        Node(int val) : data(val), prev(NULL), next(NULL) {};
+};
+
+void insertAtTail(Node *&head, int value) {
+    Node *createdNode = new Node(value);
+    if(head == NULL) {
+        head = createdNode;
+    }
+    else {
+        Node *temp = head;
+        while(temp->next!=NULL) {
+            temp = temp->next;
+        }
+        createdNode->prev = temp;
+        temp->next = createdNode;
+    }
+}
+
+void displayLL(Node *head) {
+    Node *temp = head;
+    while (temp->next != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << temp->data << endl;
+}
+
+int main() {
+    Node *head = NULL;
+    insertAtTail(head, 4);
+    insertAtTail(head, 7);
+    displayLL(head);
+}
--- linkedlist.cpp
+++ linkedlist.cpp
@@ -14,13 +14,80 @@ ListNode* createLinkedList() {
     cout << "Enter the length of linked list: ";
     cin >> n;
     ListNode *head = NULL;
+    ListNode *temp = NULL;
+    int data;
+    for(int i = 0; i < n; ++i) {
+        cout << "Enter the data for node " << i+1 << endl;
+        cin >> data;
+
+        ListNode *newNode = new ListNode(data);
+
+        if(head == NULL) {
+            head = newNode;
+        }
+        else {
+            temp->next = newNode;
+        }
+        temp = newNode;
+    }
+    return head;
 }
 
+void displayLL(ListNode *head) {
+    ListNode *temp = head;
+    while (temp->next != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << temp->data << endl;
+}
+
+void insertAtTail(ListNode* &head, int data) {
+    ListNode *createdNode = new ListNode(data);
+    if(head == NULL) {
+        head = createdNode;
+        return;
+    }
+
+    ListNode *currentNode = head;
+
+    while(currentNode->next != NULL) {
+        currentNode = currentNode->next;
+    }
+    currentNode->next = createdNode;
+}
+
+void insertAtStart(ListNode * &head, int value) {
+    ListNode *createdNode = new ListNode(value);
+
+    if(head == NULL) {
+        head = createdNode;
+        return;
+    }
+    createdNode->next = head;
+    head = createdNode;
+}
+
+bool searchLL(ListNode *head, int key) {
+    ListNode* temp = head;
+    while(temp != NULL) {
+        if(temp->data == key) {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
 
 int main() {
     ListNode *head = NULL;
-    ListNode createdNode = ListNode(3);
-    head = &createdNode;
-
-
+    // head = createLinkedList();
+    // displayLL(head);
+    
+    insertAtStart(head, 12);
+    insertAtStart(head, 13);
+    insertAtStart(head, 14);
+    displayLL(head);
+    cout << searchLL(head, 1);
 }
