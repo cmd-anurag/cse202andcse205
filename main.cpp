972a036ddfc625090b431395eba3e4d2c94d7f1d
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-09-10 20:25:30 +0530

    test

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    int arr[] = {5, 2, 8, 12, 3};
-    int n = 5;
+int count(int n) {
+    
+}
 
-    for (int i = n - 1; i >= 1; i--) {
-        for (int j = 0; j <= i-1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    return 0;
+int main() {
+    int n;
+
 }
