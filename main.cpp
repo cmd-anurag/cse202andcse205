aa4b5de9197748ad50d74c9daaa0615aca69aeab
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-10-22 15:07:40 +0530

    2583

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<queue>
+
 using namespace std;
 
 
@@ -35,13 +37,66 @@ int findHeight(Node* root) {
     return 1 + max(findHeight(root->left), findHeight(root->right));
 }
 
+queue<Node *> q;
+
+
+void printRightView(Node* root) {
+    if(!root) return;
+    q.push(root);
+
+    while(!q.empty()) {
+        int size = q.size();
+        for(int i = 0; i < size; ++i) {
+            Node* current = q.front();
+            q.pop();
+            if(i == size -1) {
+                cout << current->data << " ";
+            }
+            if(current->left) {
+                q.push(current->left);
+            }
+            if(current->right) {
+                q.push(current->right);
+            }
+        }
+    }
+}
+
+bool getPath(Node *root, int key, vector<int> &path) {
+
+    if(!root) {
+        return false;
+    }
+
+    path.push_back(root->data);
+    if(root->data == key) return true;
+
+    if(getPath(root->left, key, path) || getPath(root->left, key, path)) {
+        return true;
+    }
+    path.pop_back();
+    return false;
+}
+
+
+int lowestCommonAncestor(Node* root, int key1, int key2) {
+    vector<int> key1path;
+    vector<int> key2path;
+
+    if(!getPath(root, key1, key1path) || !getPath(root, key2, key2path)) {
+        return -1;
+    }
+
+}
+
 int main() {
-    int arr[] = {1, 2, 3, 4, 5, 6, 7,8};
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     Node* root = arrayToBinaryTree(arr, n);
 
-   cout << findHeight(root);
+    cout << findHeight(root);
+    printRightView(root);
 
     return 0;
 }
