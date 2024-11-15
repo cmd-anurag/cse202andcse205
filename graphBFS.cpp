04a7886d02b9b267be0ae09e01f8e85aa78575fe
cmd-anurag <153152868+cmd-anurag@users.noreply.github.com>
2024-11-15 19:34:59 +0530

    graph questions

--- graphBFS.cpp
+++ graphBFS.cpp
@@ -27,3 +27,73 @@ vector<int> bfsTraversal(int n, vector<vector<int>> &adj){
     
 
 }
+
+
+// neocolab shortest path bfs
+// You are using GCC
+#include<unordered_map>
+#include<vector>
+#include<queue>
+
+
+void add_edge(int adj[][100], int src, int dest)
+{
+    //Type your code
+    adj[src][dest] = 1;
+    adj[dest][src] = 1;
+}
+
+bool BFS(int adj[][100], int src, int dest, int v, int pred[], int dist[])
+{
+    //Type your code
+    queue<int> q;
+    unordered_map<int, bool> visited;
+    
+    for(int i = 0; i < v; ++i) {
+        dist[i] = INT_MAX;
+        pred[i] = -1;
+    }
+    
+    q.push(src);
+    dist[src] = 0;
+    visited[src] = true;
+    
+    while(!q.empty()) {
+        int front = q.front();
+        q.pop();
+        
+        for(int i = 0; i < v; ++i) {
+            if(adj[front][i] && !visited[i]) {
+                q.push(i);
+                dist[i] = dist[front] + 1;
+                pred[i] = front;
+                visited[i] = true;
+                
+                if(i == dest) {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void printShortestDistance(int adj[][100], int s, int dest, int v, int pred[], int dist[])
+{
+    //Type your code
+    vector<int> path;
+    int crawl = dest;
+    
+    path.push_back(crawl);
+    
+    while(pred[crawl] != -1) {
+        path.push_back(pred[crawl]);
+        crawl = pred[crawl];
+    }
+    
+    cout << "Shortest path length is: " << dist[dest] << '\n';
+    cout << "Path is: ";
+    for(int i = path.size()-1; i >= 0; --i) {
+        cout << path[i] << " ";
+    }
+}
--- graphDFS.cpp
+++ graphDFS.cpp
@@ -54,3 +54,47 @@ class Solution {
         return result;
     }
 };
+
+// Disconnected Graph
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    // Write your code here
+    unordered_map<int, vector<int>> adjlist;
+
+    for(int i = 0; i < edges.size(); ++i) {
+
+        for(int j = 0; j < edges[i].size(); ++j) {
+            adjlist[i].push_back(edges[i][j]);
+        }
+    }
+
+    unordered_map<int, bool> visited;
+    vector<vector<int>> result;
+
+    stack<int> st;
+
+    for(int i = 0; i < V; ++i) {
+        if(visited.find(i) == visited.end() || visited[i] == false) {
+            vector<int> component;
+            // clear the stack or make it local
+            st.push(i);
+            while(!st.empty()) {
+                int top = st.top();
+                st.pop();
+                if(!visited[top]) {
+                    component.push_back(top);
+                }
+                visited[top] = true;
+                for(int i = adjlist[top].size()-1; i >= 0; --i) {
+                    if(!visited[adjlist[top][i]]) {
+                        st.push(adjlist[top][i]);
+                    }
+                }
+            }
+
+            result.push_back(component);
+        }
+    }
+
+}
